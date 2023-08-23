2c0200394857e7d8a947cf814ef3ff1a4490c193
Fábio simones <simones.fabio1994@gmail.com>
2023-08-23 19:01:37 -0300

    Seller details and eager loading

--- SistemaDeVendas/Services/SellerService.cs
+++ SistemaDeVendas/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SistemaDeVendas.Data;
 using SistemaDeVendas.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SistemaDeVendas.Services
 {
@@ -29,7 +30,7 @@ namespace SistemaDeVendas.Services
 
         public Seller FindById (int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault( obj => obj.Id == id);
         }
 
         public void Remove (int id)
