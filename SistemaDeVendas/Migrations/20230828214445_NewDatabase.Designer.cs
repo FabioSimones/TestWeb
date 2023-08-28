88d1a67ab0f5a3a7d3c9eda8f2f2e1f7df9262b4
Fábio simones <simones.fabio1994@gmail.com>
2023-08-28 18:47:17 -0300

    Validation

--- SistemaDeVendas/Migrations/20230828214445_NewDatabase.Designer.cs
+++ SistemaDeVendas/Migrations/20230828214445_NewDatabase.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaDeVendas.Data;
 
 namespace SistemaDeVendas.Migrations
 {
     [DbContext(typeof(SistemaDeVendasContext))]
-    partial class SistemaDeVendasContextModelSnapshot : ModelSnapshot
+    [Migration("20230828214445_NewDatabase")]
+    partial class NewDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
